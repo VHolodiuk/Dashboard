f8ccab7e0a8d0651f3f1cf4f805ec8b61495199f
VHolodiuk <vasyl.holodiuk@beetroot.se>
2024-03-01 01:29:49 +0200

    Update Dashbord

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/dashboard",
+    basePath: "/Dashboard",
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
     images: { unoptimized: true },
