320bce8cf505b8057a8039590195753d21412884
VHolodiuk <vasyl.holodiuk@beetroot.se>
2024-03-01 01:51:08 +0200

    Test

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // basePath: "/Dashboard",
+    basePath: "/Dashboard",
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
     images: { unoptimized: true },
