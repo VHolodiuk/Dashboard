4a92501e2525ec7756a82989b3b4200b215a109c
VHolodiuk <vasyl.holodiuk@beetroot.se>
2024-03-04 17:32:47 +0200

    Update login

--- auth.config.ts
+++ auth.config.ts
@@ -10,13 +10,9 @@ export const authConfig = {
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
       if (isOnDashboard) {
         if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
+        // return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        let newUrl = nextUrl.origin + '/dashboard';
-        if (nextUrl.origin != 'http://localhost:3000') {
-          newUrl = 'https://vholodiukdashboard.vercel.app/dashboard';
-        }
-        return Response.redirect(new URL(newUrl, nextUrl));
+        return Response.redirect(new URL('/dashboard', nextUrl));
       }
       return true;
     },
